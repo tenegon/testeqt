bf2624cf02edc596c70aeeeecf197a13629b1b6a
tenegon <tenegon@hotmail.com>
2017-11-17 16:12:20 -0200

    Este commit é para colar no github um projeto qt

---
+++ teste.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[]){
+	QApplication app(argc, argv);
+	QPushButton hello("Hello, world!!!");
+	app.connect(&hello, SIGNAL(clicked()), &app, SLOT(quit()));
+	hello.show();
+	return app.exec();
+}
